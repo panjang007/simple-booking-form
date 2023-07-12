dde79410689bf957dfd4099a578664e0e00b958a
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2023-07-13 00:33:34 +0800

    update css js
    added placeholder to my username and password input field

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("usernameInput").placeholder = "Your username";
+  document.getElementById("passwordInput").placeholder = "Your password";
+});
